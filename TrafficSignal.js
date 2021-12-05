6fa0f53f8720923a455338246ddd5d66c5e25219
MayekarKirti <mayekarkirti27@gmail.com>
2021-12-05 20:35:16 +0530

    added new file

---
+++ TrafficSignal.js
@@ -0,0 +1,12 @@
+let color="red"
+let color="orange"
+let color="green"
+if(color==red){
+    console.log("Stop");
+}
+else if(color==orange){
+    console.log("Ready to Go");
+}
+else{
+    console.log("Go");
+}
